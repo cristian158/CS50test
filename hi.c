0b245c99a879f0d15fd5d7ca6c9c1fff1f4224b6
cristian <cnovoa.o@gmail.com>
2021-07-28 02:42:07 +0200

    test for C

---
+++ hi.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char c1 = 'H';
+	char c2 = 'I';
+	char c3 = '!';
+
+	printf("%c%c%c\n", c1, c2, c3);			// gives the character
+	printf("%i %i %i\n", c1, c2, c3);		// gives character id in numbers
+
+	string xxx = "Hello!";
+	printf("%s\n", xxx);
+}
